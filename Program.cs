4e780e9ac955ed930b39665e4fd676cca4c662af
Sherzod0001 <141539590+Sherzod0001@users.noreply.github.com>
2024-06-06 23:58:45 +0500

    Initialize project

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+string heightUserInput = Console.ReadLine();
+int height = Convert.ToInt32(heightUserInput);  
+
+string widthUserInput = Console.ReadLine();
+int width = Convert.ToInt32(widthUserInput);
+
+for (int i = 0; i < height; i++)
+{
+    Console.WriteLine();
+
+    for (int j = 0; j < width; j++)
+    {
+        Console.Write(" *");
+    }
+}
+
+Console.WriteLine();
+string heightTriangleUserInput = Console.ReadLine();
+int triangleHeight = Convert.ToInt32(heightTriangleUserInput); 
+
+
+for (int i = 0; i < triangleHeight; i++)
+{
+    Console.WriteLine();
+
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write(" *");
+    }
+}
